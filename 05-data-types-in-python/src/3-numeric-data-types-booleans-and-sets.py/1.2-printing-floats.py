4eed55785a6190bbdbdc44e6a9008410089d6933
Jorge Roldan <jorge.roldan@gmail.com>
2025-06-02 15:05:37 +0200

    Numeric data types

---
+++ 05-data-types-in-python/src/3-numeric-data-types-booleans-and-sets.py/1.2-printing-floats.py
@@ -0,0 +1,15 @@
+float1 = 0.0001
+float2 = 1e-05
+float3 = 1e-07
+
+# Print floats 1, 2, and 3
+print(float1)
+print(float2)
+print(float3)
+
+# Print floats 2 and 3 using the f string formatter
+print(f"{float2:f}")
+print(f"{float3:f}")
+
+# Print float 3 with a 7 f string precision
+print(f"{float3:.7f}")
